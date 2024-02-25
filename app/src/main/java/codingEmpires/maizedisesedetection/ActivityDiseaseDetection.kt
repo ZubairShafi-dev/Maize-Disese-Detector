4799f393f249082d2d569d7e97aa95ea859565b1
ZubairShafi-dev <zubair38421@gmail.com>
2024-02-26 02:14:18 +0500

    intial frontend set

---
+++ app/src/main/java/codingEmpires/maizedisesedetection/ActivityDiseaseDetection.kt
@@ -0,0 +1,11 @@
+package codingEmpires.maizedisesedetection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityDiseaseDetection : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_disease_detection)
+    }
+}
